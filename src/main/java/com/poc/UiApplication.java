11ee74c932807686bbd4a4584d36ec77905d99f0
liniyakamnya <liniyakamnya@gmail.com>
2018-01-31 23:43:59 +0300

    redirect fix

--- src/main/java/com/poc/UiApplication.java
+++ src/main/java/com/poc/UiApplication.java
@@ -2,17 +2,10 @@ package com.poc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
@@ -20,12 +13,8 @@ import java.util.Map;
 
 @SpringBootApplication
 @Controller
-//@RestController
 @EnableZuulProxy
-@EnableOAuth2Sso
-@EnableOAuth2Client
-@CrossOrigin(origins = "http://localhost:4200")
-public class UiApplication extends WebSecurityConfigurerAdapter {
+public class UiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UiApplication.class, args);
@@ -54,16 +43,16 @@ public class UiApplication extends WebSecurityConfigurerAdapter {
         return supplier;
     }
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        // @formatter:off
-        CookieCsrfTokenRepository csrfTokenRepository = CookieCsrfTokenRepository.withHttpOnlyFalse();
-        http
-                .logout().and()
-                .authorizeRequests()
-                .antMatchers("/static/index.html", "/home.html", "/", "/login", "/get-supplier/*").permitAll()
-                ;
-        // @formatter:oln
-    }
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        // @formatter:off
+//        CookieCsrfTokenRepository csrfTokenRepository = CookieCsrfTokenRepository.withHttpOnlyFalse();
+//        http
+//                .logout().and()
+//                .authorizeRequests()
+//                .antMatchers("/static/index.html", "/home.html", "/", "/login*", "/get-supplier/*").permitAll()
+//                ;
+//        // @formatter:oln
+//    }
 }
 
